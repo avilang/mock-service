7ed77e6539a07856beac534937876d3609b80227
avilang <1985945885@qq.com>
2022-04-04 17:32:53 +0800

    接口代理

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 let express = require("express");
-let config = (function () {
-  try {
-    return require("./config.local.json");
-  } catch (error) {
-    return { port: 9009 };
-  }
-})();
+let cors = require("cors");
+let { createProxyMiddleware } = require("http-proxy-middleware");
+let config = require("./config.local.json");
 let app = express();
 let port = config.port;
+let proxy = config.proxy || {};
+let filter = function (pathname, req) {
+  if (pathname === "/") return false;
+  return true;
+};
+
+app.use(cors());
+
+for (let key in proxy) {
+  if (Object.hasOwnProperty.call(proxy, key)) {
+    app.use(key, createProxyMiddleware(filter, proxy[key]));
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.all("*", function (req, res) {
+  res.send("no proxy");
 });
 
 app.listen(port, () => {
